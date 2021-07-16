4921f46d33113647bbde574f46ee4352089fe223
johnny21-9 <heartless6m@gmail.com>
2021-07-16 21:52:08 +0300

    fixes

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 import { galleryItems } from './app.js';
 
+// Refs:
 const galleryContainer = document.querySelector('.js-gallery');
 const lightboxRef = document.querySelector('.js-lightbox');
 const overlayRef = document.querySelector('.lightbox__overlay');
 const closeOverlayButton = document.querySelector('[data-action="close-lightbox"]');
 const lightboxPhotoContainer = document.querySelector('.lightbox__image');
 
+
+// Creating gallery markup:
 const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
 
@@ -28,37 +31,33 @@ function createGalleryMarkup(galleryArray) {
 };
 
 
-//open/close modal
+// Open/close modal:
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 
 function onGalleryContainerClick(event) {
     event.preventDefault();
-    const isGalleryImage = event.target.classList.contains('gallery__image');
-    if (!isGalleryImage) {
-        return;
-    }
 
     lightboxPhotoContainer.alt = event.target.alt;
     lightboxPhotoContainer.src = event.target.dataset.source;
-    openModal();
-}
 
+    openModal(event);
+}
 
 overlayRef.addEventListener('click', closeModal);
 closeOverlayButton.addEventListener('click', closeModal);
 
 function closeModal () {
-    if (!lightboxRef.classList.contains('is-open')) {
-        return
-    }
     lightboxRef.classList.toggle('is-open');
+    //Cleaning alt and src:
     lightboxPhotoContainer.alt = '';
     lightboxPhotoContainer.src = '';
 }
 
-function openModal() {
-    if (lightboxRef.classList.contains('is-open')) {
-        return
+function openModal(event) {
+    const isGalleryImage = event.target.classList.contains('gallery__image');
+    if (!isGalleryImage) {
+        return;
     }
+    
     lightboxRef.classList.toggle('is-open');
 }
